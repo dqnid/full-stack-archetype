f91286fb05999ac72c57e7f5c61e4586135d7ba8
Daniel Heras Quesada <dani.heras@hotmail.com>
2024-08-29 00:17:02 +0200

    feat(example): example list page done

--- front/src/modules/example/components/example-list/example-list.component.tsx
+++ front/src/modules/example/components/example-list/example-list.component.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@/modules/common/hooks/api/useQuery";
 import styles from "./example-list.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 type ExampleListProps = {};
 
@@ -24,11 +26,35 @@ export const ExampleList: React.FC<ExampleListProps> = ({}) => {
     timeout: 4000,
   });
 
+  if (result.isLoading) {
+    return <>Loading...</>;
+  }
+
+  if (result.isError) {
+    return <>Error</>;
+  }
+
   return (
-    <div data-testid="{example-list}" className={styles.container}>
-      {JSON.stringify(result.data)}
-      {one.isError && <h1>ERROR</h1>}
-      <h1>ONE{JSON.stringify(one.data)}</h1>
+    <div data-testid="example-list" className={styles.container}>
+      <ul>
+        {result.data?.map((example) => {
+          return (
+            <Link href={`/examples/${example.id}`}>
+              <li key={example.id}>
+                <Image
+                  src={example.image}
+                  alt="picture"
+                  width={30}
+                  height={30}
+                />
+                <span>{example.name}</span>
+                <span>{example.description}</span>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+      <div>This could be the pagination...</div>
     </div>
   );
 };
