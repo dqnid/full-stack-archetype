712ffd7ef8a4da99970db38ff41304029883175f
dqnid <dani.heras@hotmail.com>
2024-10-30 15:20:33 +0100

    fix(files): cleanup

--- front/.component.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./.module.scss";
-
-type Props = {}
-
-export const :React.FC<Props> = ({}) => {
-  return (
-    <div data-testid="{}" className={styles.container}></div>
-  )
-}
